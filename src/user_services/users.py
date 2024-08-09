644c151778aa89cfece74226adc2bdebab0e216d
Yaser Noorllahi <yaser.noorollahi@gmail.com>
2024-08-09 09:29:26 +0300

    installed fastapi and uvicorn

--- src/user_services/users.py
+++ src/user_services/users.py
@@ -1,34 +1,15 @@
-from flask import Flask, jsonify, request
-from flask_restful import Api, Resource
-from models import User
-from common.sqlite_setup import db,init_db
+from fastapi import FastAPI
 
+app=FastAPI()
 
-app =Flask(__name__)
-api = Api(app)
-init_db(app)
+@app.get('/',description='this is my first route in fastapi', deprecated=True)
+async def base_get_route():
+    return{'message':'Hello, World!'}
 
+@app.post('/')
+async def post():
+    return {'message':'hello from the post route'}
 
-
-users={}
-
-class UserResource(Resource):
-    # def post(self):
-    #     user_list= User.query.first()
-
-    #     return jsonify(user_list)
-        # user_id= request.json.get('id')
-        # user_data = request.json.get('data')
-        # users[user_id]= user_data
-        # return jsonify({'id': user_id, 'data': user_data})
-
-    def get(self):
-        user = User.query.get_or_404(1)
-        # return jsonify({'id': 's', 'username': })
-        return {user.id }
-    
-
-api.add_resource(UserResource, '/users')
-
-if __name__ == '__main__':
-    app.run(debug=True, port=50002)
+@app.put('/{id}')
+async def put():
+    return {'message':'hello from the put route'}
