7958ab8cf48bcbad9dd77599d1117f9227336a5e
Yaser Noorllahi <yaser.noorollahi@gmail.com>
2024-08-26 18:10:28 +0300

    working on docker files

--- Dockerfile
+++ Dockerfile
@@ -8,4 +8,6 @@ RUN pip install -r requirements.txt
 
 COPY . /app
 
+ENV PYTHONPATH=/app
+
 CMD ["flask","run","--host=0.0.0.0"]
--- src/common/db_setup_flask.py
+++ src/common/db_setup_flask.py
@@ -15,14 +15,17 @@ class DBSetup:
         if db_initialized:
             return
         print(colored('--- Database Initializing....','green', attrs=['bold']))
-        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/yaser/Downloads/PythonProjects/FlaskIntroduction/src/test.db'
-        print(colored('--- Database Path = Users/yaser/Downloads/PythonProjects/FlaskIntroduction/src/test.db',color='green',attrs=['bold']))
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///instance/test.db'
+        # print(colored('--- Database Path = Users/yaser/Downloads/PythonProjects/FlaskIntroduction/src/test.db',color='green',attrs=['bold']))
         app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
         db.init_app(app)
         migrate = Migrate(app,db)
         print(colored('after init app',color='red', attrs=['bold']))
         with app.app_context():
             db.create_all()
+            engine = db.engine
+            database_uri = engine.url
+            print(f"Database URI: {database_uri}")
             print(colored('inside with',color='red',attrs=['bold'])) 
         db_initialized =True
          
